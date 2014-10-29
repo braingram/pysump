97090921c9559a239984d0ae6e653f4cbd66272d
Brett Graham <brettgraham@gmail.com>
2014-10-29 16:37:55 -0400

    switching to using struct to clean stuff up

--- sump/interface.py
+++ sump/interface.py
@@ -2,27 +2,30 @@
 
 # import sys
 
+import logging
+import struct
+
 import serial
 
-from . import defaults
 from . import errors
 from . import fio
 from . import ops
 # import settings this as settings_module to avoid name conflicts
 from . import settings as settings_module
 
+logger = logging.getLogger(__name__)
+
 
 class Interface(object):
     # undivided clock rate, in Hz, from testing with OBLS
-    clock_rate = 100000000
     protocol_version = '1.0'
 
-    def __init__(self, path=None, baud=None, timeout=None):
+    def __init__(
+            self, path='/dev/ttyACM0', baud=115200,
+            timeout=None, settings=None):
         self.timeout = timeout
-        if path is None:
-            path = defaults.PATH
-        if baud is None:
-            baud = defaults.BAUD
+        if settings is None:
+            self.settings = settings_module.Settings()
         self.port = serial.Serial(path, baud, timeout=self.timeout)
         self.debug_logger = None
         self.reset()
@@ -30,218 +33,197 @@ class Interface(object):
         self.settings = None
 
     def reset(self):
-        w = self.port.write
-        w('\x00')
-        w('\x00')
-        w('\x00')
-        w('\x00')
-        w('\x00')
+        logger.debug("reset")
+        self.port.write('\x00\x00\x00\x00\x00')
 
-    def capture(self, settings=None):
+    def capture(self, send_settings=True):
         '''Request a capture.'''
-        if settings is None:
-            settings = self.settings
-        if settings is None:
-            raise errors.SettingsError("capture must have access to settings")
+        logger.debug("capture")
+        if send_settings:
+            self.send_settings()
         # get local references to objects for faster execution ..
         ufs = []
         for i in xrange(4):
-            if not (settings.channel_groups & (0b1 < i)):
+            if not (self.settings.channel_groups & (0b1 < i)):
                 ufs.append(lambda c, si=i: ord(c) << (8 * si))
 
         d = []
-        self.port.timeout = settings.timeout
+        self.port.timeout = self.settings.timeout
         self.port.write('\x01')  # start the capture
-        for _ in xrange(settings.read_count):
+        for _ in xrange(self.settings.read_count):
             v = 0
             for uf in ufs:
                 v |= uf(self.port.read(1))
             d.append(v)
 
         self.reset()  # TODO is this needed?
-        if settings.latest_first:
+        if self.settings.latest_first:
             return d[::-1]
         else:
             return d
 
     def save(self, capture, filename, meta=None):
+        logger.debug("save %s", filename)
         fio.save(capture, filename, self.settings, meta)
 
     def id_string(self):
         '''Return device's SUMP ID string.'''
+        logger.debug("id_string")
         self.port.write('\x02')
+        # TODO check protocol version here
         val = self.port.read(4)  # 4 bytes as a small-endian int
         return val[::-1]
 
     def xon(self):
+        logger.debug("xon")
         self.port.write('\x11')
 
     def xoff(self):
+        logger.debug("xoff")
         self.port.write('\x13')
 
-    def _trace_control(self, legend):
-        if self.debug_logger is None:
-            return self.port.write
-        else:
-            w = self.port.write
-            logger = self.debug_logger
-            logger.write('\n' + legend + ' \t')
-
-            def tw(data):
-                logger.write('%s ' % (bin(ord(data))[2:].zfill(8),))
-                logger.flush()
-                w(data)
-            return tw
-
     def _send_trigger_mask(self, stage, mask):
-        # w = self.port.write
-        w = self._trace_control('Trigger mask')
-        w(chr(0xC0 | (stage << 2)))
-        w(chr(mask & 0xFF))
-        w(chr((mask >> 8) & 0xFF))
-        w(chr((mask >> 16) & 0xFF))
-        w(chr((mask >> 24) & 0xFF))
+        logger.debug("send_trigger_mask %s %s", stage, mask)
+        #w = self.port.write
+        #w = self._trace_control('Trigger mask')
+        msg = struct.pack('<Bi', 0xC0 | (stage << 2), mask)
+        self.port.write(msg)
+        #w(chr(0xC0 | (stage << 2)))
+        #w(chr(mask & 0xFF))
+        #w(chr((mask >> 8) & 0xFF))
+        #w(chr((mask >> 16) & 0xFF))
+        #w(chr((mask >> 24) & 0xFF))
 
-    def send_trigger_mask_settings(self, settings):
-        # w = self.port.write
-        w = self._trace_control('Trigger mask')
-        for stage in xrange(defaults.MAX_TRIGGER_STAGES):
-            m = settings.trigger_mask[stage]
-            w(chr(0xC0 | (stage << 2)))
-            w(chr(m & 0xFF))
-            w(chr((m >> 8) & 0xFF))
-            w(chr((m >> 16) & 0xFF))
-            w(chr((m >> 24) & 0xFF))
-
-    def _send_trigger_values(self, stage, values):
-        # w = self.port.write
-        w = self._trace_control('Trigger values')
-        w(chr(0xC1 | (stage << 2)))
-        w(chr(values & 0xFF))
-        w(chr((values >> 8) & 0xFF))
-        w(chr((values >> 16) & 0xFF))
-        w(chr((values >> 24) & 0xFF))
-
-    def send_trigger_values_settings(self, settings):
-        # w = self.port.write
-        w = self._trace_control('Trigger values')
-        for stage in xrange(defaults.MAX_TRIGGER_STAGES):
-            v = settings.trigger_values[stage]
-            w(chr(0xC1 | (stage << 2)))
-            w(chr(v & 0xFF))
-            w(chr((v >> 8) & 0xFF))
-            w(chr((v >> 16) & 0xFF))
-            w(chr((v >> 24) & 0xFF))
+    def _send_trigger_value(self, stage, value):
+        logger.debug("send_trigger_value %s %s", stage, value)
+        #w = self.port.write
+        #w = self._trace_control('Trigger values')
+        msg = struct.pack('<Bi', 0xC1 | (stage << 2), value)
+        self.port.write(msg)
+        #w(chr(0xC1 | (stage << 2)))
+        #w(chr(values & 0xFF))
+        #w(chr((values >> 8) & 0xFF))
+        #w(chr((values >> 16) & 0xFF))
+        #w(chr((values >> 24) & 0xFF))
 
     def _send_trigger_configuration(
             self, stage, delay, channel, level, start, serial):
-        # w = self.port.write
-        w = self._trace_control('Trigger config')
-        w(chr(0xC2 | (stage << 2)))
-        d = delay
-        w(chr(d & 0xFF))
-        w(chr((d >> 8) & 0xFF))
-        c = channel
-        w(chr(((c & 0x0F) << 4) | level))
-        w(chr((start << 3) | (serial << 2) | ((c & 0x10) >> 4)))
-
-    def send_trigger_configuration_settings(self, settings):
-        # w = self.port.write
-        w = self._trace_control('Trigger config')
-        for stage in xrange(defaults.MAX_TRIGGER_STAGES):
-            w(chr(0xC2 | (stage << 2)))
-            d = settings.trigger_delay[stage]
-            w(chr(d & 0xFF))
-            w(chr((d >> 8) & 0xFF))
-            c = settings.trigger_channel[stage]
-            w(chr(((c & 0x0F) << 4) | settings.trigger_level[stage]))
-            w(chr((settings.trigger_start[stage] << 3) | (
-                settings.trigger_serial[stage] << 2) | ((c & 0x10) >> 4)))
+        logger.debug(
+            "send_trigger_configuration %s %s %s %s %s %s",
+            stage, delay, channel, level, start, serial)
+        msg = struct.pack(
+            '<BHBB',
+            0xC2 | (stage << 2),
+            delay,
+            ((channel & 0x0F) << 4) | level,
+            (start << 3) | (serial << 2) | ((channel & 0x10) >> 4))
+        self.port.write(msg)
+        #w = self.port.write
+        #w = self._trace_control('Trigger config')
+        #w(chr(0xC2 | (stage << 2)))
+        #d = delay
+        #w(chr(d & 0xFF))
+        #w(chr((d >> 8) & 0xFF))
+        #c = channel
+        #w(chr(((c & 0x0F) << 4) | level))
+        #w(chr((start << 3) | (serial << 2) | ((c & 0x10) >> 4)))
 
     def send_divider_settings(self, settings):
-        # w = self.port.write
-        w = self._trace_control('Divider')
-        w('\x80')
+        logger.debug("send_divider_settings %s", settings.divider)
         d = settings.divider - 1  # offset 1 correction for SUMP hardware
-        w(chr(d & 0xFF))
-        w(chr((d >> 8) & 0xFF))
-        w(chr((d >> 16) & 0xFF))
-        w('\x00')
+        msg = struct.pack('<cHBx', '\x80', d & 0xFFFF, d >> 16)
+        self.port.write(msg)
+        #w = self.port.write
+        ##w = self._trace_control('Divider')
+        #w('\x80')
+        #d = settings.divider - 1  # offset 1 correction for SUMP hardware
+        #w(chr(d & 0xFF))
+        #w(chr((d >> 8) & 0xFF))
+        #w(chr((d >> 16) & 0xFF))
+        #w('\x00')
 
     def send_read_and_delay_count_settings(self, settings):
-        # w = self.port.write
-        w = self._trace_control('Read/Delay')
-        w('\x81')
-        # factor 4 correction for SUMP hardware
+        logger.debug("send_read_and_delay_count_settings")
         r = (settings.read_count + 3) >> 2
-        w(chr(r & 0xFF))
-        w(chr((r >> 8) & 0xFF))
-        # factor 4 correction for SUMP hardware
         d = (settings.delay_count + 3) >> 2
-        w(chr(d & 0xFF))
-        w(chr((d >> 8) & 0xFF))
+        msg = struct.pack('<cHH', '\x81', r, d)
+        self.port.write(msg)
+        #w = self.port.write
+        ##w = self._trace_control('Read/Delay')
+        #w('\x81')
+        ## factor 4 correction for SUMP hardware
+        #r = (settings.read_count + 3) >> 2
+        #w(chr(r & 0xFF))
+        #w(chr((r >> 8) & 0xFF))
+        ## factor 4 correction for SUMP hardware
+        #d = (settings.delay_count + 3) >> 2
+        #w(chr(d & 0xFF))
+        #w(chr((d >> 8) & 0xFF))
 
     def send_flags_settings(self, settings):
-        # w = self.port.write
-        w = self._trace_control('Flags')
-        w('\x82')
-        w(chr((settings.inverted << 7)
-              | (settings.external << 6)
-              | (settings.channel_groups << 2)
-              | (settings.filter << 1)
-              | settings.demux
-              ))
-        # disable RLE compression, alternate number scheme, test modes
-        w('\x00')
-        w('\x00')
-        w('\x00')
+        logger.debug("send_flag_settings")
+        msg = struct.pack(
+            '<cBxxx', '\x82',
+            (settings.inverted << 7) | (settings.external << 6) |
+            (settings.channel_groups << 2) | (settings.filter << 1) |
+            settings.demux)
+        self.port.write(msg)
+        #w = self.port.write
+        ##w = self._trace_control('Flags')
+        #w('\x82')
+        #w(chr((settings.inverted << 7)
+        #      | (settings.external << 6)
+        #      | (settings.channel_groups << 2)
+        #      | (settings.filter << 1)
+        #      | settings.demux
+        #      ))
+        ## disable RLE compression, alternate number scheme, test modes
+        #w('\x00')
+        #w('\x00')
+        #w('\x00')
 
-    def send_settings(self, settings):
+    def send_settings(self):
         """
         The order of things in this function are CRITICAL
         """
-        self.send_divider_settings(settings)
-        trigger_enable = settings.trigger_enable
+        logger.debug("send_settings")
+        self.send_divider_settings(self.settings)
+        trigger_enable = self.settings.trigger_enable
         if trigger_enable == 'None':
             # send always-trigger trigger settings
-            for stage in xrange(defaults.MAX_TRIGGER_STAGES):
+            for stage in xrange(self.settings.trigger_max_stages):
                 self._send_trigger_configuration(stage, 0, 0, 0, True, False)
                 self._send_trigger_mask(stage, 0)
                 self._send_trigger_values(stage, 0)
         elif trigger_enable == 'Simple':
             # set settings from stage 0, no-op for stages 1..3
             self._send_trigger_configuration(
-                0, settings.trigger_delay[0], settings.trigger_channel[0],
-                0, True, settings.trigger_serial[0])
-            self._send_trigger_mask(0, settings.trigger_mask[0])
-            self._send_trigger_values(0, settings.trigger_values[0])
-            for stage in xrange(1, defaults.MAX_TRIGGER_STAGES):
+                0, self.settings.trigger_stages[0].delay,
+                self.settings.trigger_stages[0].channel,
+                0, True, self.settings.trigger_stages[0].serial)
+            self._send_trigger_mask(0, self.settings.trigger_stages[0].mask)
+            self._send_trigger_values(0, self.settings.trigger_stages[0].value)
+            for stage in xrange(1, self.self.settings.trigger_max_stages):
                 self._send_trigger_configuration(stage, 0, 0, 0, False, False)
                 self._send_trigger_mask(stage, 0)
                 self._send_trigger_values(stage, 0)
         elif trigger_enable == 'Complex':
-            for stage in xrange(defaults.MAX_TRIGGER_STAGES):
+            for (i, stage) in enumerate(self.settings.trigger_stages):
                 # OLS needs things in this order
-                self._send_trigger_mask(stage, settings.trigger_mask[stage])
-                self._send_trigger_values(stage, settings.trigger_values[stage])
+                self._send_trigger_mask(i, stage.mask)
+                self._send_trigger_values(i, stage.value)
                 self._send_trigger_configuration(
-                    stage, settings.trigger_delay[stage],
-                    settings.trigger_channel[stage],
-                    settings.trigger_level[stage],
-                    settings.trigger_start[stage],
-                    settings.trigger_serial[stage],
-                )
+                    i, stage.delay, stage.channel, stage.level, stage.start,
+                    stage.serial)
         else:
             raise errors.TriggerEnableError
-        self.send_read_and_delay_count_settings(settings)
-        self.send_flags_settings(settings)
-
-    def set_logfile(self, logfile):
-        self.debug_logger = logfile
+        self.send_read_and_delay_count_settings(self.settings)
+        self.send_flags_settings(self.settings)
 
     def query_metadata(self):
         '''Return metadata identifying the SUMP device,
         firmware, version, etc.'''
+        logger.debug("query_metadata")
         result = []
         self.reset()
         r = self.port.read
@@ -281,6 +263,7 @@ class Interface(object):
         return result
 
     def close(self):
+        logger.debug("close")
         self.port.close()
         self.port = None
 
--- sump/settings.py
+++ sump/settings.py
@@ -1,74 +1,49 @@
 #!/usr/bin/env python
 
-from . import defaults
+
+class TriggerStage(object):
+    def __init__(self, **kwargs):
+        self.mask = 0
+        self.value = 0
+        self.delay = 0
+        self.level = 0
+        self.channel = 0
+        self.serial = 0
+        self.start = 0
+        for kw in kwargs:
+            setattr(self, kw, kwargs[kw])
 
 
-class Settings (object):
-    '''Sampling and trigger parameters.'''
-    # undivided clock rate, in Hz, from testing with OBLS
-    clock_rate = 100000000
-
-    def __init__(self):
-        self.default()
-
-    def default(self):
-        '''Non-impossible default settings.'''
-        # general settings ..
+class Settings(object):
+    def __init__(self, **kwargs):
+        self.clock_rate = 100000000
         self.timeout = None
         self.latest_first = True
-        self.divider = 2			# default sampling rate 50MHz
-        self.read_count = 4096		# default sampling size
-        self.delay_count = 2048		# default before/after 50/50
-        self.external = False			# True for external trigger
-        self.inverted = False			# True to invert external trigger
-        # true to filter out glitches shorter than 1/(200MHz)
+        self.divider = 2
+        self.read_count = 4096
+        self.delay_count = 2048
+        self.external = False
+        self.inverted = False
         self.filter = False
-        self.demux = False			# True for double-speed sampling
-        self.channel_groups = 0x0  # default all channel groups
-
+        self.demux = False
+        self.channel_groups = 0x0
         self.trigger_enable = 'None'
-        # trigger settings, by stage ..
-        stages = defaults.MAX_TRIGGER_STAGES
-        self.trigger_mask = [0] * stages			# 32-bit mask for trigger channels
-        # 32-bit match-readings for trigger channels
-        self.trigger_values = [0] * stages
-        self.trigger_delay = [0] * stages			# post-trigger delay in samples
-        # user-preferred units for trigger_delay display
-        self.trigger_delay_unit = [0] * stages
-        # level at which trigger stage is armed
-        self.trigger_level = [0] * stages
-        self.trigger_channel = [0] * stages		# channel for serial trigger
-        # default parallel trigger testing
-        self.trigger_serial = [False] * stages
-        # default immediate start from stage 0
-        self.trigger_start = [True] + [False] * (stages - 1)
+        self.trigger_stages = [
+            TriggerStage(level=i) for i in xrange(self.trigger_max_stages)]
 
-    def copy(self, other):
-        '''Copy these settings to another instance.'''
-        other.divider = self.divider
-        other.read_count = self.read_count
-        other.delay_count = self.delay_count
-        other.external = self.external
-        other.inverted = self.inverted
-        other.filter = self.filter
-        other.demux = self.demux
-        other.channel_groups = self.channel_groups
+    @property
+    def trigger_max_stages(self):
+        return len(self.trigger_stages)
 
-        other.trigger_enable = self.trigger_enable
+    @trigger_max_stages.setter
+    def trigger_max_stages(self, n):
+        self.trigger_stages = [TriggerStage(level=i) for i in xrange(n)]
 
-        # trigger settings, by stage ..
-        other.trigger_mask[:] = self.trigger_mask
-        other.trigger_values[:] = self.trigger_values
-        other.trigger_delay[:] = self.trigger_delay
-        other.trigger_delay_unit[:] = self.trigger_delay_unit
-        other.trigger_level[:] = self.trigger_level
-        other.trigger_channel[:] = self.trigger_channel
-        other.trigger_serial[:] = self.trigger_serial
-        other.trigger_start[:] = self.trigger_start
-
-    def get_sample_rate(self):
-        '''Return the sample rate called for by these settings.'''
+    @property
+    def sample_rate(self):
         rate = int(self.clock_rate / self.divider)
         if self.demux:
             rate *= 2
         return rate
+
+    # TODO set properties for read and delay count to allow auto-calculation
