7a09c406dc95222946f922ca1f2cee496de7c9e4
Brett Graham <brettgraham@gmail.com>
2015-10-06 16:54:41 -0400

    sump2.ops.parse

--- sump2/ops/parse.py
+++ sump2/ops/parse.py
@@ -17,8 +17,28 @@ if settings[key] is a tuple = (start bit, length)
 if settings[key] is a single value = bit index
 """
 
+import numpy
 
-def parse(capture, settings):
-    """
-    """
-    pass
+
+def unpack(data, spec):
+    if isinstance(spec, (tuple, list)):
+        if len(spec) != 2:
+            raise ValueError("spec must be of len 2 [!=%s]" % (len(spec), ))
+        start, length = spec
+        return (data >> start) & (2 ** length - 1)
+    elif isinstance(spec, dict):
+        raise NotImplementedError("Complex unpacking not yet supported")
+    else:
+        return (data >> int(spec)) & 0b1
+
+
+def parse(capture, settings=None, **kwargs):
+    if settings is None:
+        settings = {}
+    settings.update(kwargs)
+    c = numpy.array(capture, dtype='int64')  # to allow unsafe shifting
+    dt = [(k, 'int64') for k in sorted(settings)]
+    r = numpy.empty(len(c), dtype=dt)
+    for k in settings:
+        r[k] = unpack(c, settings[k])
+    return r
